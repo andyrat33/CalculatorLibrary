bce778594e92d39d89aa6c3d30e96fe79530c634
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2021-02-23 14:50:28 +0000

    fixed spaces removed TODO

--- calculator.py
+++ calculator.py
@@ -30,9 +30,7 @@ def square(first_term):
 def cube(first_term):
     return first_term ** 3
 
+
 def power(first_term, second_term):
     return first_term ** second_term
-
-# TODO add test for power
-
 
